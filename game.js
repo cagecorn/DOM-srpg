d89b85223b86360e52232979f31d04512af015f2
cagecorn <lmcbv@naver.com>
2025-07-21 19:16:53 +0900

    1
    1

--- game.js
+++
@@ -1,7 +0,0 @@
-import { GameEngine } from './engines/GameEngine.js';
-
-// start game when DOM is ready
-window.addEventListener('DOMContentLoaded', () => {
-    const myGame = new GameEngine();
-    myGame.start();
-});

4663cb17abe072327fe407f2ddc6e7eac0cb4da6
cagecorn <lmcbv@naver.com>
2025-07-22 03:52:25 +0900

    Add initial class and monster data

---
+++ data/monster.js
@@ -0,0 +1,19 @@
+/**
+ * 게임 내 몬스터에 대한 데이터를 정의합니다.
+ * 각 몬스터는 고유한 스탯을 가집니다.
+ */
+export const monsters = {
+    zombie: {
+        name: '좀비',
+        stats: {
+            strength: 12,     // 힘
+            agility: 5,       // 민첩
+            endurance: 15,    // 인내
+            intelligence: 1,  // 지능
+            wisdom: 3,        // 지혜
+            valor: 0,         // 용맹
+            luck: 2,          // 운
+            movement: 2       // 이동
+        }
+    }
+};
